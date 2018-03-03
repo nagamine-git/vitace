8214c1308c17c540cc815a4ab40a3c42e3e31a46
nagaminenot <nagamine@slogan.jp>
2018-03-03 21:11:24 +0900

    新規追加・同期まで

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ITreeOptions } from 'angular-tree-component';
+import { ITreeOptions, TreeComponent } from 'angular-tree-component';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'; // 追加
 import { Observable } from 'rxjs/Observable'; // 追加
 import { Story } from './story';
@@ -10,9 +10,11 @@ import { Story } from './story';
 })
 export class StoryComponent implements OnInit {
 
+  tree: TreeComponent;
   name: String = '';
+  storiesBoard: AngularFireList<{}>;
   storiesRef: AngularFireList<{}>;
-  stories: Observable<any[]>;
+  stories: Observable<{}>;
   options: ITreeOptions = {
     allowDrag: (node) => {
       return true;
@@ -21,10 +23,12 @@ export class StoryComponent implements OnInit {
     nodeClass: (node) => {
       return 'card';
     },
-    useCheckbox: true
+    useCheckbox: true,
+    idField: '_id',
   };
 
   constructor(db: AngularFireDatabase) {
+    this.storiesBoard = db.list('/boards/0');
     this.storiesRef = db.list('/boards/0/stories');
     this.stories = this.storiesRef.valueChanges();
   }
@@ -32,12 +36,32 @@ export class StoryComponent implements OnInit {
   ngOnInit() {
   }
 
-  save(name) {
-    this.storiesRef.push({'name': this.name})
+  // save(tree) {
+  //   console.log(Object.keys(tree.treeModel.nodes));
+  //   this.storiesRef.push({'name': this.name})
+  //   .then(() => this.name = '');
+  // }
+
+  save(tree) {
+    tree.treeModel.nodes.push({name: this.name});
+    tree.treeModel.update();
+    this.storiesBoard.set( 'stories', tree.treeModel.nodes)
     .then(() => this.name = '');
   }
 
   changeValue(name) {
     this.name = name;
+  }
+
+  // 未完成
+  update(name) {
+    this.storiesRef.update( 'L6fJOmWwvBG93-PXn29', {'name': name});
+    this.stories.subscribe((res: Response) => {
+      console.log(res);
+    });
+  }
+  updateTree(tree) {
+    console.log(tree.treeModel.nodes);
+    this.storiesBoard.set( 'stories', tree.treeModel.nodes);
   }
 }
